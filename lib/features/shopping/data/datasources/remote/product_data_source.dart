643e60c2d37fa7543146a69fca3093d24c06f555
Than Thongsuebsaeng <6242043726@cbs.chula.ac.th>
2025-02-17 07:20:35 +0700

    fix: typo

--- lib/features/shopping/data/datasources/remote/product_data_source.dart
+++ lib/features/shopping/data/datasources/remote/product_data_source.dart
@@ -17,7 +17,7 @@ class ProductDataSourceImpl implements ProductDataSource {
   @override
   Future<Map<String, dynamic>> fetchProducts(String? nextCursor) async {
     nextCursor ??= "";
-    final response = await dio.get('${ApiConfig.baseUrl}/${ApiConfig.products}Path?cursor=$nextCursor');
+    final response = await dio.get('${ApiConfig.baseUrl}/${ApiConfig.products}?cursor=$nextCursor');
 
     if (response.statusCode == 200) {
 
